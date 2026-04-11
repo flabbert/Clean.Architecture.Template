73da505aad69cdf2b0de6973e408060370abc93e
flabbert <gmkarto@iddqd.no>
2026-04-11 09:50:27 +0200

    update local nuget publishing

--- publish.ps1
+++ publish.ps1
@@ -14,7 +14,7 @@ $csproj = 'Flabbert.Clean.Template.csproj'
 [xml]$xml = Get-Content $csproj
 
 if (-not $Version) {
-    $current = [version]$xml.Project.PropertyGroup.PackageVersion
+    $current = [version](Select-Xml -Xml $xml -XPath '//PackageVersion').Node.InnerText
     switch ($Bump) {
         'major' { $Version = "$($current.Major + 1).0.0" }
         'minor' { $Version = "$($current.Major).$($current.Minor + 1).0" }
@@ -26,7 +26,7 @@ if (-not $Version) {
 }
 
 Write-Host "Version: $Version"
-$xml.Project.PropertyGroup.PackageVersion = $Version
+(Select-Xml -Xml $xml -XPath '//PackageVersion').Node.InnerText = $Version
 $xml.Save((Resolve-Path $csproj))
 
 dotnet pack -c Release /p:PackageVersion=$Version
